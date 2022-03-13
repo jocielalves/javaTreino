dd921a2ae1d32d4d18dd62a02306795e704dd127
jociel <jocielalvesdejesus@gmail.com>
2022-03-13 14:08:51 -0300

    atualizando

--- src/Treino.java
+++ src/Treino.java
@@ -84,13 +84,13 @@ public class Treino {
             System.out.print("Digite a senha de acesso: ");
             senha = ler.next();
 
-            if (senha.equals("2ff45")) {
+            if (senha.equals("2002")) {
                 System.out.println("Acesso Permitido");
                 break;
             } else {
                 System.out.println("Senha Inválida");
             }
-        } while ( senha != "2002");
+        } while (!senha.equals(" "));
 
         }
 }
